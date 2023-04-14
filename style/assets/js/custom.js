0a2096dc5635df36964c06b8dacb43ac8bac5e2b
吕文博 <lv_wenbo@foxmail.com>
2023-04-14 16:47:35 +0800

    move

---
+++ style/assets/js/custom.js
@@ -0,0 +1,258 @@
+(function(a) {
+	a(document).ready(function() {
+		a("#circle-1").circleProgress({
+			value: 0.9,
+			size: 100,
+			fill: {
+				color: "#55aa7f"
+			}
+		});
+		a("#circle-2").circleProgress({
+			value: 0.94,
+			fill: {
+				color: "#55aa7f"
+			}
+		});
+		a("#circle-3").circleProgress({
+			value: 0.96,
+			fill: {
+				color: "#55aa7f"
+			}
+		});
+		a("#circle-4").circleProgress({
+			value: 0.9,
+			fill: {
+				color: "#55aa7f"
+			}
+		});
+		a("#circle-5").circleProgress({
+			value: 0.9,
+			size: 100,
+			fill: {
+				color: "#55aa7f"
+			}
+		});
+		a("#circle-6").circleProgress({
+			value: 0.9,
+			fill: {
+				color: "#55aa7f"
+			}
+		});
+		a("#circle-7").circleProgress({
+			value: 0.96,
+			fill: {
+				color: "#55aa7f"
+			}
+		});
+		a("#circle-8").circleProgress({
+			value: 0.75,
+			fill: {
+				color: "#ffaa7f"
+			}
+		});
+		a("#circle-9").circleProgress({
+			value: 0.9,
+			fill: {
+				color: "#55aa7f"
+			}
+		});
+		a("#circle-10").circleProgress({
+			value: 0.9,
+			fill: {
+				color: "#55aa7f"
+			}
+		});
+		a("#circle-11").circleProgress({
+			value: 0.87,
+			fill: {
+				color: "#55aa7f"
+			}
+		});
+		a("#circle-12").circleProgress({
+			value: 0.9,
+			fill: {
+				color: "#55aa7f"
+			}
+		});
+		a("#circle-13").circleProgress({
+			value: 0.94,
+			fill: {
+				color: "#55aa7f"
+			}
+		});
+		a("#circle-14").circleProgress({
+			value: 0.9,
+			fill: {
+				color: "#55aa7f"
+			}
+		});
+		a("#circle-15").circleProgress({
+			value: 0.9,
+			fill: {
+				color: "#55aa7f"
+			}
+		});
+		a("#circle-16").circleProgress({
+			value: 0.9,
+			fill: {
+				color: "#55aa7f"
+			}
+		});
+		a("#circle-17").circleProgress({
+			value: 0.9,
+			fill: {
+				color: "#55aa7f"
+			}
+		});
+		a("#circle-18").circleProgress({
+			value: 0.9,
+			fill: {
+				color: "#55aa7f"
+			}
+		});
+		a("#circle-19").circleProgress({
+			value: 0.9,
+			fill: {
+				color: "#55aa7f"
+			}
+		});
+		a("#circle-20").circleProgress({
+			value: 0.75,
+			fill: {
+				color: "#ffaa7f"
+			}
+		});
+		a("#circle-21").circleProgress({
+			value: 0.75,
+			fill: {
+				color: "#ffaa7f"
+			}
+		});
+		a("#circle-22").circleProgress({
+			value: 0.9,
+			fill: {
+				color: "#55aa7f"
+			}
+		});
+		a("#circle-23").circleProgress({
+			value: 0.9,
+			fill: {
+				color: "#55aa7f"
+			}
+		});
+		a("#circle-24").circleProgress({
+			value: 0.9,
+			fill: {
+				color: "#55aa7f"
+			}
+		});
+		a("#circle-25").circleProgress({
+			value: 0.99,
+			fill: {
+				color: "#55aa7f"
+			}
+		});
+		a("#circle-26").circleProgress({
+			value: 0.92,
+			fill: {
+				color: "#55aa7f"
+			}
+		});
+		a("#circle-27").circleProgress({
+			value: 0.9,
+			fill: {
+				color: "#55aa7f"
+			}
+		});
+		a("#circle-28").circleProgress({
+			value: 0.9,
+			fill: {
+				color: "#55aa7f"
+			}
+		});
+		a("#circle-29").circleProgress({
+			value: 0.75,
+			fill: {
+				color: "#ffaa7f"
+			}
+		});
+		a("#circle-30").circleProgress({
+			value: 0.9,
+			fill: {
+				color: "#55aa7f"
+			}
+		});
+		a("#circle-31").circleProgress({
+			value: 0.9,
+			fill: {
+				color: "#55aa7f"
+			}
+		});
+		a("#circle-32").circleProgress({
+			value: 0.6,
+			fill: {
+				color: "#ff8000"
+			}
+		});
+		a("#circle-33").circleProgress({
+			value: 0.9,
+			fill: {
+				color: "#55aa7f"
+			}
+		});
+		a("#circle-34").circleProgress({
+			value: 0.9,
+			fill: {
+				color: "#55aa7f"
+			}
+		});
+		a("#circle-35").circleProgress({
+			value: 0.99,
+			fill: {
+				color: "#55aa7f"
+			}
+		});
+		a("#circle-36").circleProgress({
+			value: 0.99,
+			fill: {
+				color: "#55aa7f"
+			}
+		});
+		a("#circle-37").circleProgress({
+			value: 0.91,
+			fill: {
+				color: "#55aa7f"
+			}
+		});
+		a("#circle-38").circleProgress({
+			value: 0.9,
+			fill: {
+				color: "#55aa7f"
+			}
+		});
+		a("#circle-39").circleProgress({
+			value: 0.9,
+			fill: {
+				color: "#55aa7f"
+			}
+		});
+		a("#circle-40").circleProgress({
+			value: 0.9,
+			fill: {
+				color: "#55aa7f"
+			}
+		});
+		a("#circle-41").circleProgress({
+			value: 0.9,
+			fill: {
+				color: "#55aa7f"
+			}
+		});
+		a("#circle-42").circleProgress({
+			value: 0.9,
+			fill: {
+				color: "#55aa7f"
+			}
+		});
+	})
+}(jQuery));
